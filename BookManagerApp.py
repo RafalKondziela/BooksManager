53eb49c2c9110bb219182fd02c81ff4171c0d098
Rafał Kondziela <rafal.kondziela@gmail.com>
2019-08-20 20:03:42 +0200

    Adding the library to store books and option to add book to it(still work in progress - not working correctly)

--- BookManagerApp.py
+++ BookManagerApp.py
@@ -3,6 +3,7 @@ from Books import *
   
 
 loop = "go"
+library = {0:"test"}
 
 while(loop == "go"):
     key = input("Hello in Book Manager type command to perform action or 'help' to get list of commands \n\n")
@@ -17,6 +18,18 @@ while(loop == "go"):
     elif(key == "exit"):
         print("Bye")
         break
+
+    elif(key == "list"):
+        print(library)
+
+    elif(key =="add"):
+        length = len(library)
+        book = Book(
+        str(input("Add Author: ")),
+        str(input("Add Title: ")),
+        int(input("Add number of pages: ")))
+        book.addBook(library, length, book) 
+        
 
     else :
         print("Wrong command try again")
--- Books.py
+++ Books.py
@@ -1,8 +1,20 @@
 class Book:
 
-    def __init__(self, author, title):
+    def __init__(self, author, title, number_of_pages):
         self.author = str(author)
         self.title = str(title)
+        self.number_of_pages = str(number_of_pages)
+
+    def __str__(self):
+        return "{} {} {}".format(self.author,self.title,self.number_of_pages)
 
 
-def addBook(book):
+
+    def show_Title(book):
+        print(book.title)
+
+    def addBook(library, length,book):
+        library[int(length + 1)] = book
+
+        
+    
